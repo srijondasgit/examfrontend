8c44d86d3f813c04dce690198283414050ccba5a
akashmore83386 <67957668+akashmore83386@users.noreply.github.com>
2021-07-16 21:16:18 +0530

    Update App.js

--- exammodule/src/App.js
+++ exammodule/src/App.js
@@ -1,14 +1,12 @@
 import React from 'react';
-//import Form from './components/Form';
-import PersonList from './components/PersonList';
+import Form from './components/Form';
+// import PersonList from './components/PersonList';
 
-// let cors = require("cors");
-// app.use(cors());
 
 function App() {
   return (
     <div className="App">
-      <PersonList />
+      <Form />
     </div>
   );
 }
