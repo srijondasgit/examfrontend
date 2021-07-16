ee614acf50dac926618da4e526eed01d15fe0460
srijondasgit <61958188+srijondasgit@users.noreply.github.com>
2021-07-15 22:31:36 -0700

    Update App.js

--- exammodule/src/App.js
+++ exammodule/src/App.js
@@ -1,23 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+//import Form from './components/Form';
+import PersonList from './components/PersonList';
+
+// let cors = require("cors");
+// app.use(cors());
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://github.co.in"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <PersonList />
     </div>
   );
 }
