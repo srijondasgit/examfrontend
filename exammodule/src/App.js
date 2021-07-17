c1750e3d55a8405cee857e30be3c6f5033b5b57f
akashmore83386 <67957668+akashmore83386@users.noreply.github.com>
2021-07-17 23:00:45 +0530

    Update App.js

--- exammodule/src/App.js
+++ exammodule/src/App.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import Form from './components/Form';
-// import PersonList from './components/PersonList';
+import Login from './components/Login';
+
 
 
 function App() {
   return (
     <div className="App">
+      <Login />
+      <br />
       <Form />
     </div>
   );
