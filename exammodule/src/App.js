c2275b7e6b7a619026dd41e101681c83dd869c07
akashmore83386 <67957668+akashmore83386@users.noreply.github.com>
2021-07-29 03:05:30 +0530

    Update App.js

--- exammodule/src/App.js
+++ exammodule/src/App.js
@@ -3,25 +3,71 @@ import Form from './components/Form';
 import Login from './components/Login';
 import Reset from './components/Reset';
 import AddTestHeader from './components/AddTestHeader';
+import AddQuestion from './components/AddQuestion';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import Navbar from './components/Navbar';
 
-
-
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <Form />
-      <br />
-      <hr />
-      <Login />
-      <br />
-      <hr />
-      <Reset />
-      <br />
-      <hr />
-      <AddTestHeader />
-   
-    </div>
+
+    <div className="wrapper">
+    
+    <BrowserRouter>
+    <Navbar />
+      <Switch>
+        
+        <Route path="/user/authenticate">
+          <Login />
+        </Route>
+
+        <Route path="/user/register">
+          <Form />
+        </Route>
+
+        <Route path="/password/reset">
+          <Reset />
+        </Route>
+      </Switch>
+    </BrowserRouter>
+  </div>
+
+
+    // <div>
+    //   <BrowserRouter>
+    //     <div>
+          
+    //       <Route path="/" exact component={Form} />
+    //       <Route path="/Login" component={Login} />
+    //       <Route path="/Reset" component={Reset} />
+    //     </div>
+    //   </BrowserRouter>
+    // </div>
   );
-}
+};
 
 export default App;
+
+
+
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <Form />
+//       <br />
+//       <hr />
+//       <Login />
+//       <br />
+//       <hr />
+//       <Reset />
+//       <br />
+//       <hr />
+//       <AddTestHeader />
+//       <br />
+//       <hr />
+//       <AddQuestion />
+//     </div>
+//   );
+// }
+
+// export default App;
