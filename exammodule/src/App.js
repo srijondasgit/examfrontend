b0c3f5637c484dc54ad4258c1df731b7f28910e2
akashmore83386 <67957668+akashmore83386@users.noreply.github.com>
2021-09-27 21:44:05 +0530

    Update App.js

--- exammodule/src/App.js
+++ exammodule/src/App.js
@@ -9,63 +9,9 @@ import AddTestHeader from './components/AddTestHeader';
 import AddQuestion from './components/AddQuestion';
 import ShowTestList from './components/ShowTestList';
 
-// import { BrowserRouter, Route, Switch } from 'react-router-dom';
-
-// const App = () => {
-//   return (
-
-  //   <div className="wrapper">
-    
-  //   <BrowserRouter>
-  //   <Navbar />
-  //     <Switch>
-        
-  //       <Route path="/user/authenticate">
-  //         <Login />
-  //       </Route>
-
-  //       <Route path="/user/register">
-  //         <Form />
-  //       </Route>
-
-  //       <Route path="/password/reset">
-  //         <Reset />
-  //       </Route>
-  //     </Switch>
-  //   </BrowserRouter>
-  // </div>
-
-
-    // <div>
-    //   <BrowserRouter>
-    //     <div>
-          
-    //       <Route path="/" exact component={Form} />
-    //       <Route path="/Login" component={Login} />
-    //       <Route path="/Reset" component={Reset} />
-    //     </div>
-    //   </BrowserRouter>
-    // </div>
-//   );
-// };
-
-// export default App;
-
-const id = localStorage.getItem('id')
 
 function App() {
   return (
-    // <div className="App">
-    //   <Form />
-    //   <br />
-    //   <hr />
-    //   <Login />
-    //   <br />
-    //   <hr />
-    //   <Reset />
-    //   <br />
-    //   <hr />
-    // </div>
     <div>
     <Header />
     <Route path="/">
@@ -91,3 +37,5 @@ function App() {
 }
 
 export default App;
+
+
