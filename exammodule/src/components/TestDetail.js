e7ddfd8686c46f341f02df25d34c5f5585544f83
akashmore83386 <67957668+akashmore83386@users.noreply.github.com>
2021-10-03 03:58:42 +0530

    Add files via upload

---
+++ exammodule/src/components/TestDetail.js
@@ -0,0 +1,32 @@
+import React, { useEffect, useState } from "react";
+import { useParams, useHistory } from "react-router-dom";
+import axios from "axios";
+
+const TestDetail = () => {
+  const { id } = useParams();
+  const { push } = useHistory();
+  const [test, setTest] = useState({});
+  useEffect(() => {
+    const fetch = async () => {
+      try {
+        const { data } = await axios.get(`/user/testId/${id}/getTest`);
+        setTest(data);
+      } catch (err) {
+        console.error(err);
+      }
+    };
+    fetch();
+  }, []);
+  return (
+    <article>
+      <h1>ID - {test.id}</h1>
+      <p>Test Name - {test.testName}</p>
+      <p>School Name - {test.schoolName}</p>
+      <p>Class Name - {test.className}</p>
+      <br />
+      <button onClick={() => push("/")}>Go back</button>
+    </article>
+  );
+};
+
+export default TestDetail;
