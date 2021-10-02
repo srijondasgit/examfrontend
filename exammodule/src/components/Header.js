1298b276a776c507f32665117eead8ecf5684ab1
akashmore83386 <67957668+akashmore83386@users.noreply.github.com>
2021-10-03 03:56:28 +0530

    Update Header.js

--- exammodule/src/components/Header.js
+++ exammodule/src/components/Header.js
@@ -1,24 +1,25 @@
 import React from 'react';
-import Link from './Link';
+import {Link} from 'react-router-dom';
 
 const Header = () => {
   return (
     <div className="ui secondary pointing menu">
-      <Link href="/" className="item">
+      <Link to="/" className="item">
         Sign Up
       </Link>
-      <Link href="/user/authenticate" className="item">
+      <Link to="/user/authenticate" className="item">
         Log In
       </Link>
-      <Link href="/teacher/addTestHeader" className="item">
+      <Link to="/teacher/addTestHeader" className="item">
         Test Header
       </Link>
-      <Link href="/teacher/addQuestion" className="item">
+      <Link to="/teacher/addQuestion" className="item">
         Add Question
       </Link>
-      <Link href="/user/allTests" className="item">
+      <Link to="/user/allTests" className="item">
         Show all test ids
       </Link>
+     
     </div>
   );
 };
