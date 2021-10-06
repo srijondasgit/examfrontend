94845680a4ce674feaf9bf3ac11ad288825c461e
Srijon <srijondas@gmail.com>
2021-10-06 16:20:02 -0700

    fix to questions variables in useState

--- exammodule/src/components/TestDetail.js
+++ exammodule/src/components/TestDetail.js
@@ -6,11 +6,14 @@ const TestDetail = () => {
   const { id } = useParams();
   const { push } = useHistory();
   const [test, setTest] = useState({});
+  // within useState square brackets must be used to recognize questions as an array
+  const [questions, setQuestions] = useState([]); 
   useEffect(() => {
     const fetch = async () => {
       try {
         const { data } = await axios.get(`/user/testId/${id}/getTest`);
         setTest(data);
+        setQuestions(data.questions);
       } catch (err) {
         console.error(err);
       }
@@ -23,9 +26,12 @@ const TestDetail = () => {
       <p>Test Name - {test.testName}</p>
       <p>School Name - {test.schoolName}</p>
       <p>Class Name - {test.className}</p>
-      <p>Index - {JSON.stringify(test.questions[0].index)}</p>
-      <p>Question - {JSON.stringify(test.questions[0].questionText)}</p>
-      <p>Score - {JSON.stringify(test.questions[0].score)}</p>
+      {/* Div below returns all the questions */}
+      <div>
+        {questions.map(function(q, idx){
+           return (<li key={idx}>{q.questionText}</li>)
+         })}
+      </div>
       <br />
       <button onClick={() => push("/user/allTests")}>Go back</button>
     </article>
