d05bd5dc32b21523c28ee44b6c796ca6db928569
akashmore83386 <67957668+akashmore83386@users.noreply.github.com>
2021-10-16 00:39:11 +0530

    Add files via upload

---
+++ exammodule/src/components/SubmissionsTeacher.js
@@ -0,0 +1,54 @@
+import React,{useState,useEffect} from "react";
+import axios from "axios";
+import { useParams, useHistory } from "react-router-dom";
+import { Table } from "react-bootstrap";
+
+
+function SubmissionsTeacher() {
+    const { id } = useParams();
+
+    const [submissions, setSubmissions] = useState([]);
+
+    const config = {
+        headers: {
+          Authorization: "Bearer " + localStorage.getItem("token"),
+        },
+      };
+
+    useEffect(async()=>{
+        const response = await axios.get(`/teacher/testId/${id}/getSubmissions`, config);
+        const submissions = response.data;
+        console.log(submissions)
+        setSubmissions(submissions);
+    },[])
+    return (
+        <div>
+            <ol>
+                {submissions.map((u)=>{
+                     return (
+                        <div key={id}>
+                          <div>
+                            <Table striped bordered hover>
+                              <thead>
+                                <tr>
+                                  <th>Id</th>
+                                  <th>Student Name</th>
+                                </tr>
+                              </thead>
+                              <tbody>
+                                <tr>
+                                  <td>{u.id}</td>
+                                  <td>{u.studentName}</td>
+                                </tr>
+                              </tbody>
+                            </Table>
+                          </div>
+                        </div>
+                      );
+                })}
+            </ol>
+        </div>
+    )
+}
+
+export default SubmissionsTeacher;
