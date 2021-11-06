42a85669ee65dfdd52ae93253904e3a7c9105f30
akashmore83386 <67957668+akashmore83386@users.noreply.github.com>
2021-11-06 15:03:02 +0530

    Update GetStudentTestIds.js

--- exammodule/src/components/GetStudentTestIds.js
+++ exammodule/src/components/GetStudentTestIds.js
@@ -1,6 +1,7 @@
 import React, {useState, useEffect} from "react";
 import axios from "axios";
 import {Link} from 'react-router-dom';
+import {Table} from "react-bootstrap";
 
  function GetStudentTestIds() {
     const [ids, setIds] = useState([]);
@@ -20,12 +21,54 @@ import {Link} from 'react-router-dom';
     return (
         <div>
             <ol>
-                {ids.map((id) => (
-                     <div key={id.id}>
-                        <Link to={`/user/testId/${id.id}/getStudentTest`}>
-                        {id.id}
-                        </Link>
+                {ids.map((id, idx) => (
+                     <div key={idx}>
+                     <div>
+                       <Table striped bordered hover>
+                         <thead>
+                           <tr>
+                             <th>Id</th>
+                             <th>Test Name</th>
+                             <th>School Name</th>
+                             <th>Class Name</th>
+                          </tr>
+                         </thead>
+                         <tbody>
+                           <tr>
+                             <td><Link to={`/user/testId/${id.id}/getStudentTest`}>{id.id}</Link></td>
+                             <th>{id.testName}</th>
+                             <td>{id.schoolName}</td>
+                             <td>{id.className}</td>
+                               {/* <td><Link>
+                                     <ModalUpdateSubmissionHeader id={id} qid={q.id} config={config} />
+                                   </Link>
+                             </td> */}
+                             
+                             {/* <td> */}
+                               {/* <Link
+                                 onClick={() => {
+                                   axios.get(
+                                     `/teacher/testId/${id}/getSubmissions`,
+                                     config
+                                   );
+                                 }}
+                                 to={`/teacher/testId/${id}/getSubmissions`}
+                               >
+                                getSubmissions
+                               </Link> */}
+                             {/* </td> */}
+                            
+                           </tr>
+                         </tbody>
+                       </Table>
+                     </div>
                    </div>
+       
+                //      <div key={id.id}>
+                //         <Link to={`/user/testId/${id.id}/getStudentTest`}>
+                //         {id.id}
+                //         </Link>
+                //    </div>
                 ))}
             </ol>
         </div>
