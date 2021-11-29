189dbd703de8cfd1c87afe30fa0ce0b2863ba7ef
akashmore83386 <67957668+akashmore83386@users.noreply.github.com>
2021-11-29 18:13:51 +0530

    Add files via upload

--- exammodule/src/components/Logout.js
+++ exammodule/src/components/Logout.js
@@ -1,29 +1,24 @@
-import React, { useState, useEffect } from "react";
-import axios from "axios";
-import {useHistory} from "react-router-dom";
-import { Spinner, Button } from "react-bootstrap";
-import Form from "./Form";
-
- const Logout = () => {
-   const [profileRole, setProfileRole] = useState([]);
-  const { push } = useHistory();
-
-  localStorage.removeItem("token");
-
-  return(
+import React from "react";
+import { Button } from "react-bootstrap";
+function Logout() {
+  const clear = window.localStorage.clear();
+  return (
     <div className="container">
-              <h5 className="d-inline-block mb-3"> </h5>
-      <div style={{ padding: 10, margin: 20, background: "rgb(245 245 245)", width: '100%', borderRadius: 10, border: 1, borderColor: '#ccc'}}>
-        <div className="form-group">
-          <label htmlFor="Logout Message" className="mt-2">
-            User has been logged out
-          </label>
-          </div>
-          </div>
+      <h5 className="d-inline-block mb-3"> </h5>
+
+      <div
+        style={{
+          background: "rgb(245 245 245)",
+          borderRadius: 10,
+          border: 1,
+          borderColor: "#ccc",
+        }}
+      >
+        You have logout, Please Login <a href="/user/authenticate">here</a>
+        {/* <Button onClick={clear}>Logout</Button> */}
+      </div>
     </div>
- 
   );
+}
 
- }
-
- export default Logout;
+export default Logout;
